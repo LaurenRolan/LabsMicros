e6f5e4994a7121838a2b5842830aebefb3b55850
LaurenRolan <lauren_rolan@yahoo.com.br>
2018-04-26 09:27:50 -0300

    Update intcount.c

--- Lab05/intcount/intcount.c
+++ Lab05/intcount/intcount.c
@@ -16,8 +16,16 @@ void quit(int signal)
 int main(int argc,char * argv[])
 {
         unsigned char c;
+        
+        /*A estrutura pollpd se dá por:
+           struct pollfd {
+               int   fd;         // file descriptor 
+               short events;     // requested events 
+               short revents;    // returned events 
+           };*/
         static struct pollfd pfd;
         int i;
+        
         struct sigaction act;
 
         /* IO4 = gpio6 */
@@ -49,13 +57,26 @@ int main(int argc,char * argv[])
         puts("Waiting for interrupt. Press ^C to abort...");
         
         act.sa_handler=quit;
+        /*SIGINT is the interrupt signal. The terminal sends it to the foreground process when the user presses ctrl-c. 
+        The default behavior is to terminate the process, but it can be caught or ignored. The intention is to provide 
+        a mechanism for an orderly, graceful shutdown. */
         sigaction(SIGINT,&act,NULL);
+        
+        /*SIGTERM is the termination signal. The default behavior is to terminate the process, but it also can be caught 
+        or ignored. The intention is to kill the process, gracefully or not, but to first allow it a chance to cleanup.*/
         sigaction(SIGTERM,&act,NULL);
 
         for(i=0;run;i++)
         {
+                /*poll() performs a similar task to select(2): it waits for one of a
+                set of file descriptors to become ready to perform I/O.
+                Specifying a negative value in timeout means an infinite timeout.  
+                Specifying a timeout of zero causes poll() to return immediately, even 
+                if no file descriptors are ready.*/
+                //Argumentos em ordem: fds, número de fds, timeout;
                 poll(&pfd,1,-1);
 
+                //Move-se para o início do arquivo e lê um byte
                 lseek(pfd.fd,0,SEEK_SET);        
                 read(pfd.fd,&c,1);
 
